039daaac7b0abb645564dcd81e85e371d00dd74a
Gabriel Anacik <anacik.gabriel@gmail.com>
2022-06-05 16:09:38 +0200

    added event listener on input

--- src/Components/SearchBar/index.tsx
+++ src/Components/SearchBar/index.tsx
@@ -2,10 +2,14 @@ import "./styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
   const [input, setInput] = useState("");
+  const navigate = useNavigate();
+
+  const handlePressKey = (e: React.KeyboardEvent) =>
+    e.key === "Enter" && navigate("/search/" + input);
 
   return (
     <div className="searchbar">
@@ -14,6 +18,7 @@ const SearchBar = () => {
         placeholder="Search"
         type="text"
         onChange={(e) => setInput(e.target.value)}
+        onKeyUp={(e) => handlePressKey(e)}
       />
       <Link to={`/search/${input}`}>
         <button className="search-button">
